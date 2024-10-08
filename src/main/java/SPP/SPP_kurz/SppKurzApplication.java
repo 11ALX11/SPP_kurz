8c71060d8beebfbe802a1d91e3205c9607fc316c
Aleksey <11_alx_11@ro.ru>
2024-10-08 09:06:29 +0300

    init as springboot with maven

---
+++ src/main/java/SPP/SPP_kurz/SppKurzApplication.java
@@ -0,0 +1,13 @@
+package SPP.SPP_kurz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SppKurzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SppKurzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SPP/SPP_kurz/SppKurzApplicationTests.java
@@ -0,0 +1,13 @@
+package SPP.SPP_kurz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SppKurzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
